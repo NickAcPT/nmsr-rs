9afa6d17bba338ac8b4e9e160430d9cf83c5d5f9
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-11-01 17:58:48 +0000

    Add part organizer

---
+++ nmsr-multi-parts-organizer/src/main.rs
@@ -0,0 +1,43 @@
+use std::path::Path;
+use walkdir::WalkDir;
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let types = ["FullBody", "FrontFull"];
+
+    for x in WalkDir::new("parts") {
+        let entry = x?;
+        let path = entry.path();
+        let file_name = path.file_stem().ok_or_else(|| anyhow::anyhow!("Invalid path"))?;
+
+        // Get last word in file name
+        let last_word = file_name.to_str()
+            .and_then(|s| s.split(' ').last()).ok_or_else(|| anyhow::anyhow!("Invalid path"))?;
+
+        if !path.is_file() {
+            continue;
+        }
+        if !types.contains(&last_word) {
+            continue;
+        }
+        let new_parent = Path::new("parts").join(last_word);
+        // Take all but last word
+        let new_file_name = file_name.to_str()
+            .map(|s| s.split(' '))
+            .map(|s| s.take_while(|&s| s != last_word).collect::<Vec<_>>().join(" "))
+            .ok_or_else(|| anyhow::anyhow!("Invalid path"))?;
+
+
+        // Insert new_parent between parts the file name
+        let old_path_without_parent = path.strip_prefix("parts")?.with_file_name(&new_file_name).with_extension("png");
+        let new_path = new_parent.join(old_path_without_parent);
+
+        println!("{:?} -> {:?}", path, new_path);
+
+        // Move file
+        std::fs::create_dir_all(&new_path.parent().ok_or_else(|| anyhow::anyhow!("Invalid path"))?)?;
+        std::fs::rename(path, new_path)?;
+    }
+
+    Ok(())
+}

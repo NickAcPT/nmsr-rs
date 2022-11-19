911fc8ae2c271bfc198a672112daf8cd5f0cd341
Wyatt Herkamp <wherkamp@gmail.com>
2022-11-19 13:33:15 -0500

    Possible Embed Fix

--- nmsr-aas/src/routes/index_route.rs
+++ nmsr-aas/src/routes/index_route.rs
@@ -1,7 +1,15 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, head, HttpResponse, Responder};
+use actix_web::http::header::CONTENT_LENGTH;
 
+pub const INDEX_HTML: &str = include_str!("static/index.html");
+pub const VERGEN_SHA: &str = env!("VERGEN_GIT_SHA");
 #[get("/")]
 pub(crate) async fn index() -> impl Responder {
-    HttpResponse::Ok().body(include_str!("static/index.html")
-        .replace("{{commit}}", env!("VERGEN_GIT_SHA")))
+    HttpResponse::Ok().body(INDEX_HTML
+        .replace("{{commit}}", VERGEN_SHA))
+}
+#[head("/")]
+pub(crate) async fn head() -> impl Responder {
+    let string = INDEX_HTML.replace("{{commit}}", VERGEN_SHA);
+    HttpResponse::Ok().append_header((CONTENT_LENGTH, string.len())).finish()
 }

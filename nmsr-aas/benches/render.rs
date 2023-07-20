e6210005aad2454bca293e826e1123a35d606896
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-07-20 12:46:03 +0100

    hmmmm

--- nmsr-aas/benches/render.rs
+++ nmsr-aas/benches/render.rs
@@ -3,14 +3,29 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use std::time::Duration;
+use rust_embed::RustEmbed;
+use vfs::EmbeddedFS;
 
 use nmsr_lib::parts::manager::PartsManager;
 use nmsr_lib::rendering::entry::RenderingEntry;
 use nmsr_lib::vfs::PhysicalFS;
 
+
+#[derive(RustEmbed, Debug)]
+#[folder = "../parts/"]
+struct FullBodyBiggerParts;
+
+#[derive(RustEmbed, Debug)]
+#[folder = "../run/parts/fullbody_old_2023/"]
+struct FullBodyParts;
+
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let manager = black_box(PartsManager::new(
-        &PhysicalFS::new(".\\run\\parts\\fullbody").into(),
+    let bigger_manager = black_box(PartsManager::new(
+        &EmbeddedFS::<FullBodyBiggerParts>::new().into(),
+    ))
+    .expect("Failed to load parts");
+    let normal_manager = black_box(PartsManager::new(
+        &EmbeddedFS::<FullBodyParts>::new().into(),
     ))
     .expect("Failed to load parts");
     let entry = black_box(RenderingEntry::new(
@@ -26,13 +41,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     .expect("Failed to create entry");
 
     c.bench_function("fullbody", |b| {
-        b.iter(|| black_box(black_box(&entry).render(black_box(&manager)).expect("Bruh")))
+        b.iter(|| black_box(black_box(&entry).render(black_box(&normal_manager)).expect("Bruh")))
+    });
+    c.bench_function("fullbody (bigger)", |b| {
+        b.iter(|| black_box(black_box(&entry).render(black_box(&bigger_manager)).expect("Bruh")))
     });
 }
 
 criterion_group!(
     name = benches;
-    config = Criterion::default().measurement_time(Duration::from_secs(30));
+    config = Criterion::default().measurement_time(Duration::from_secs(35));
     targets = criterion_benchmark
 );
 criterion_main!(benches);

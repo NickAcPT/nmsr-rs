e621000dfc66357993bea272e7a9f3777f9ba8d5
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-08-14 23:13:50 +0100

    Remove everything.. We're starting from scratch.

--- nmsr-aas/src/routes/mod.rs
+++
@@ -1,4 +0,0 @@
-pub(crate) mod get_skin_route;
-pub(crate) mod index_route;
-pub(crate) mod model;
-pub(crate) mod render_body_route;
--- nmsr-aas/src/utils/mod.rs
+++
@@ -1,7 +0,0 @@
-pub(crate) mod errors;
-#[cfg(feature = "tracing")]
-pub(crate) mod tracing_headers;
-#[cfg(feature = "tracing")]
-pub(crate) mod tracing_span;
-
-pub(crate) type Result<T> = std::result::Result<T, errors::NMSRaaSError>;

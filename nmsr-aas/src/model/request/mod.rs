e6210006b066c1d50af3a5f7b646649d8563cde0
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-08-30 20:34:33 +0100

    Lighting

--- nmsr-aas/src/model/request/mod.rs
+++ nmsr-aas/src/model/request/mod.rs
@@ -197,12 +197,17 @@ impl RenderRequest {
         }
 
         let camera = self.get_camera();
-        let yaw = if (camera.get_yaw().abs() - 180.0).abs() < f32::EPSILON {
-            camera.get_yaw() - 90.0
+        let abs = (camera.get_yaw().abs() - 180.0).abs();
+        let yaw = if abs < 0.01 {
+            camera.get_yaw().abs() + 90.0
         } else if camera.get_yaw().is_sign_positive() {
             camera.get_yaw()
         } else {
-            camera.get_yaw() + 90.0
+            if camera.get_yaw() <= -90.0 {
+                camera.get_yaw()
+            } else {
+                camera.get_yaw() + 90.0
+            }
         };
 
         let aligned_yaw = ((yaw + 180.0) / 90.0).floor() * 90.0;

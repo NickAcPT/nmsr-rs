e6210004cce54a8dd8c9deca9d32628ff155d62d
NickAc <32451103+NickAcPT@users.noreply.github.com>
2024-03-16 12:45:34 +0000

    vergen is actually stupid

--- nmsr-aas/build.rs
+++ nmsr-aas/build.rs
@@ -1,8 +1,8 @@
 use std::error::Error;
-use vergen::EmitBuilder;
+use vergen_gitcl::{Emitter, GitclBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Emit the instructions to the cargo build script (currently, just the current git sha hash)
-    EmitBuilder::builder().git_sha(true).emit()?;
+    Emitter::default().add_instructions(&GitclBuilder::default().sha(true).build()?)?;
     Ok(())
 }
